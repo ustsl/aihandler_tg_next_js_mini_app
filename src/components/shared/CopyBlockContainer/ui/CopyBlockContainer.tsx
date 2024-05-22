d6ad47d39f6e37352a8d63988eb8ed914355144a
ustsl <w@imvo.site>
2024-05-22 15:14:31 +0300

    upd

--- src/components/shared/CopyBlockContainer/ui/CopyBlockContainer.tsx
+++ src/components/shared/CopyBlockContainer/ui/CopyBlockContainer.tsx
@@ -2,16 +2,21 @@ import styles from './copyBlockContainer.module.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
 
 
 export const CopyBlockContainer = ({ children, textToCopy }: { children: React.ReactNode, textToCopy: string }) => {
-
+    const [isCopied, setIsCopied] = useState(false);
 
     const handleCopy = (textToCopy: string) => {
-
         navigator.clipboard.writeText(textToCopy)
             .then(() => {
                 console.log('Text copied to clipboard');
+                setIsCopied(true);
+                setTimeout(() => {
+                    setIsCopied(false);
+                }, 1500);
             })
             .catch(err => {
                 console.error('Failed to copy text: ', err);
@@ -26,6 +31,11 @@ export const CopyBlockContainer = ({ children, textToCopy }: { children: React.R
                 </div>
             </div>
             {children}
+            {isCopied && (
+                <div className={styles.copiedPopup}>
+                    Copied!
+                </div>
+            )}
         </div>
-    )
+    );
 };
